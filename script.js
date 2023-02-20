d6150c988041ff91915049ccead127c184e8bf07
DaniilChebotarev <119631891+DaniilChebotarev@users.noreply.github.com>
2023-02-20 20:53:25 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const wrapper = document.querySelector('.wrapper')
 const container = document.querySelector('.container')
 const searchInput = document.querySelector('.search');
 const searchOptions = document.querySelector('.options');
-const btnClose = document.querySelector('.btn-close')
+
 
 function debounce(fn, debounceTime) {
     let timer
@@ -16,66 +16,59 @@ function debounce(fn, debounceTime) {
 
 
 
-searchInput.addEventListener('keyup', debounce(async (e) => {
+searchInput.addEventListener('input', debounce(async (e) => {
     if (e.target.value.length == 0) {
         let links = document.querySelectorAll('.dropdown-content');
         links.forEach(i => i.remove());
     } else {
         searchOptions.innerHTML = ''
-        fetch(`https://api.github.com/search/repositories?q=${e.target.value}&per_page=5`)
+        const res = await fetch(`https://api.github.com/search/repositories?q=${e.target.value}&per_page=5`)
+        if (res.ok) {
+            const response = res.json()
+            const data = await response;
 
-            .then(response => {
-                if (response.ok) {
-                    response.json()
-                    .then(data => {
-                        let links = document.querySelectorAll('.dropdown-content');
-                        links.forEach(i => i.remove());
-                        
-                        data.items.forEach(items => {
-                            let links = document.createElement('li');
-                            links.classList.add('dropdown-content');
-                            
-                            links.addEventListener('click', (e) => {
-                                const name = items.name;
-                                const owner = items.owner.login;
-                                const stars = items.stargazers_count;
-                                
-                                const chosen = document.createElement('div');
-                                chosen.classList.add('chosen');
-                                container.appendChild(chosen)
+            data.items.forEach(item => {
+                let links = document.createElement('li');
+                links.classList.add('dropdown-content');
 
-                                const buttonClose = document.createElement('button');
-                                buttonClose.classList.add('btn-close')
+                links.addEventListener('click', (e) => {
+                    const name = item.name;
+                    const owner = item.owner.login;
+                    const stars = item.stargazers_count;
 
-                                chosen.insertAdjacentElement('afterend', buttonClose)
-                                
-                                const text = `<b>Name</b>: ${name}<br><b>Owner</b>: ${owner}<br><b>Stars</b>:${stars}`;
+                    const chosen = document.createElement('div');
+                    chosen.classList.add('chosen');
+                    container.appendChild(chosen)
 
-                                buttonClose.addEventListener('click', (e) => {
-                                    chosen.remove();
-                                    buttonClose.remove()
-                                })
-                                chosen.insertAdjacentHTML('afterbegin', text)
-                            }, {once: true})
+                    const buttonClose = document.createElement('button');
+                    buttonClose.classList.add('btn-close')
 
-                            links.textContent = items.name;
-                            searchOptions.appendChild(links)
-                        })
+                    chosen.insertAdjacentElement('afterend', buttonClose)
 
-                        
+                    const text = `<b>Name</b>: ${name}<br><b>Owner</b>: ${owner}<br><b>Stars</b>:${stars}`;
+
+                    buttonClose.addEventListener('click', (e) => {
+                        chosen.remove();
+                        buttonClose.remove()
                     })
+                    chosen.insertAdjacentHTML('afterbegin', text)
+                }, { once: true })
 
-                }
-            })
+                links.textContent = item.name;
+                searchOptions.appendChild(links)
+            });
+
+        }
+
 
 
     }
 }, 500))
 
-searchInput.addEventListener('keydown', function(e) {
+searchInput.addEventListener('keydown', function (e) {
     if (e.which === 32) {
-      e.preventDefault();
+        e.preventDefault();
     }
-  });
+});
 
-const links = searchOptions.querySelectorAll('.dropdown-content');
+
